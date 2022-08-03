96380ec021e54b94ab82a2677cf122fec67a6073
Yujean Cho <eugenie8@kakao.com>
2022-08-03 16:19:50 +0900

    [UPDATE] Add Memo Class, Dummy data

---
+++ MemoSwift/Model/Model.swift
@@ -0,0 +1,24 @@
+//
+//  Model.swift
+//  MemoSwift
+//
+//  Created by Yujean Cho on 2022/08/03.
+//
+
+import Foundation
+
+class Memo {
+    var content: String
+    var insertDate: Date
+    
+    init(content: String) {
+        self.content = content
+        insertDate = Date() // 현재의 시간을 받으므로, 별도의 파라미터로 받지 않아도 된다.
+    }
+    
+    // Dummy Data
+    static var dummyMemoList = [
+        Memo(content: "Lorem Ipsum"),
+        Memo(content: "Subscribe + 👍🏻 + ♥️")
+    ]
+}
